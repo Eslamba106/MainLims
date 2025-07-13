8673c72e3660b77837029754b279967e85084f3a
Eslam <eslambadawy25688@gmail.com>
2025-07-14 01:05:00 +0300

    sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -146,7 +146,7 @@
                             <span class="hide-menu">{{ translate('result_management') }} </span>
                         </a>
                         <ul aria-expanded="false" class="collapse  first-level">
-                            @can('show_all_results')
+                            @can('all_results')
                                 <li class="sidebar-item">
                                     <a href="{{ route('admin.result') }}" class="sidebar-link">
                                         <i class="mdi mdi-email"></i>
