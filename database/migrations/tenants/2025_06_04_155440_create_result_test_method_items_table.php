14b5e56304384792f5eb4b37fd16aa2a904457c3
Eslam <eslambadawy25688@gmail.com>
2025-09-14 21:45:05 +0300

    subscription v1

--- database/migrations/tenants/2025_06_04_155440_create_result_test_method_items_table.php
+++ database/migrations/tenants/2025_06_04_155440_create_result_test_method_items_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('result_test_method_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('result_test_method_id')->constrained('result_test_methods')->onDelete('cascade');
-            $table->foreignId('result_id')->constrained('results')->onDelete('cascade'); 
-            $table->foreignId('test_method_item_id')->constrained('test_method_items')->onDelete('cascade');
+            $table->integer('result_test_method_id');
+            $table->integer('result_id'); 
+            $table->integer('test_method_item_id');
             $table->string('result')->nullable();
             $table->string('status')->default('in_range'); 
             $table->string('acceptance_status')->default('pending'); 
