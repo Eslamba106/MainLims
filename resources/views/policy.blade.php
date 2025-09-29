ec568ce8195b671e5c590dd2c325836c36a0801a
Eslam Badawy <133673836+Eslamba106@users.noreply.github.com>
2025-09-29 14:25:57 +0300

    Create policy.blade.php

---
+++ resources/views/policy.blade.php
@@ -0,0 +1,278 @@
+<!-- =========
+
+ Template Name: Play
+ Author: UIdeck
+ Author URI: https://uideck.com/
+ Support: https://uideck.com/support/
+ Version: 1.1
+
+========== -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>{{ translate('Lims_Stage') }}</title>
+
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
+    <meta name="description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://uideck.com/play/">
+    <meta property="og:title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
+    <meta property="og:description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
+    <meta property="og:image" content="https://uideck.com/wp-content/uploads/2021/09/play-meta-bs.jpg">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://uideck.com/play/">
+    <meta property="twitter:title" content="Play - Free Open Source HTML Bootstrap Template by UIdeck">
+    <meta property="twitter:description" content="Play - Free Open Source HTML Bootstrap Template by UIdeck Team">
+    <meta property="twitter:image" content="https://uideck.com/wp-content/uploads/2021/09/play-meta-bs.jpg">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/svg" />
+
+    <!-- ===== All CSS files ===== -->
+    <link rel="stylesheet" href="{{ asset(main_path() . 'landing/assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset(main_path() . 'landing/assets/css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset(main_path() . 'landing/assets/css/lineicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset(main_path() . 'landing/assets/css/ud-styles.css') }}" />
+</head>
+.
+
+<body>
+    <!-- ====== Header Start ====== -->
+    <header class="ud-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="{{ asset(main_path() . 'assets/images/logo.png') }}" alt="Logo" width="50px"
+                                height="50px" />
+                        </a>
+                        <button class="navbar-toggler">
+                            <span class="toggler-icon"> </span>
+                            <span class="toggler-icon"> </span>
+                            <span class="toggler-icon"> </span>
+                        </button>
+
+                        <div class="navbar-collapse">
+                            <ul id="nav" class="navbar-nav mx-auto">
+                                <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#home">Home</a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#features">Features</a>
+                                </li>
+                                {{-- <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#about">About</a>
+                                </li> --}}
+                                <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#pricing">Pricing</a>
+                                </li>
+                                {{-- <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#team">Team</a>
+                                </li> --}}
+                                <li class="nav-item">
+                                    <a class="ud-menu-scroll" href="#contact">Contact</a>
+                                </li>
+                                {{-- <li class="nav-item nav-item-has-children">
+                                    <a href="javascript:void(0)"> Pages </a>
+                                    <ul class="ud-submenu">
+                                        <li class="ud-submenu-item">
+                                            <a href="about.html" class="ud-submenu-link">
+                                                About Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="pricing.html" class="ud-submenu-link">
+                                                Pricing Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="contact.html" class="ud-submenu-link">
+                                                Contact Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="blog.html" class="ud-submenu-link">
+                                                Blog Grid Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="blog-details.html" class="ud-submenu-link">
+                                                Blog Details Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="login.html" class="ud-submenu-link">
+                                                Sign In Page
+                                            </a>
+                                        </li>
+                                        <li class="ud-submenu-item">
+                                            <a href="404.html" class="ud-submenu-link">404 Page</a>
+                                        </li>
+                                    </ul>
+                                </li> --}}
+                            </ul>
+                        </div>
+
+                        {{-- <div class="navbar-btn d-none d-sm-inline-block">
+                            <a href="login.html" class="ud-main-btn ud-login-btn">
+                                Sign In
+                            </a>
+                            <a class="ud-main-btn ud-white-btn" href="javascript:void(0)">
+                                Sign Up
+                            </a>
+                        </div> --}}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+سياسة الخصوصية
+يدرك موقع (منصة الكيمياء) أهمية خصوصية وحماية بياناتك، وبالتالي نلتزم بالحفاظ على سرية المعلومات الحساسة وحماية حقوق جميع زوار هذا الموقع، وقد أعددنا هذه السياسة وبشكل محدد لتوضيح جميع ما يلي:
+
+الموافقة على سياسة الخصوصية
+
+استخدامك للموقع وتطبيقه يعني موافقتك على سياسة الخصوصية هذه، والضوابط والشروط التي تحكم الموقع والتطبيق. فإذا لم تكن موافقاً على هذه السياسة يجب عليك عدم استخدام أي من الخدمات المقدمة من خلال موقع (منصة الكيمياء).
+
+
+
+من يقوم بجمع بيانات المستخدمين؟
+
+بمجرد زيارتك للموقع chemstage.com ، يقوم الخادم الخاص بالموقع الذي يعمل بشكل تلقائي، بتسجيل عنوان بروتوكول شبكة الإنترنت IP الخاص بك (عنوان بروتوكول شبكة الإنترنت الخاص بك هو الرقم الخاص بجهاز الكمبيوتر الذي تستخدمه، والذي يسمح للأجهزة الأخرى الموصولة بشبكة الإنترنت بتحديد وجهة البيانات الصادرة عنها، وجمع بعض المعلومات مثل نوع المتصفح ومحرك البحث، وتاريخ ووقت الزيارة والعنوان URL الخاص بأي موقع إلكتروني تتم منه إحالتك إلى موقع chemstage.com، ولكن من دون التعرف على هويتك الشخصية).
+
+
+
+الروابط بالمواقع الأخرى والأطراف الثالثة على شبكة الإنترنت
+
+ينطبق نهج الخصوصية هذا على موقع (منصة الكيمياء) فقط. وإذا ما انتقلت إلى موقع آخر من خلال (منصة الكيمياء)، يتعين عليك قراءة نهج الخصوصية الخاص بذلك الموقع؛ لتحديد ممارسات أمن المعلومات وسياسة الخصوصية المعتمدة هناك.
+
+
+
+ما هي البيانات التي يتم جمعها؟
+
+1- ملفات تعريف الارتباط (Cookies)
+
+قد يقوم الموقع بتخزين ما يسمى بملفات تعريف الارتباط “Cookies” على الكمبيوتر الخاص بك عندما تقوم بزيارة الموقع. إن ملفات تعريف الارتباط “Cookies” هي جزء من البيانات التي تحددك كمستخدم بشكل فريد، كما يمكن استخدامها لتطوير معرفتك بالموقع ولفهم قاعدة مستخدم هذا الموقع على نحو أفضل.
+
+يتم إعداد معظم المتصفحات في البداية لقبول ملفات تعريف الارتباط، كما يمكنك إعادة إعداد المتصفح الخاص بك لرفض كافة ملفات تعريف الارتباط أو للتنبيه عندما يتم إرسال ملفات تعريف الارتباط. كما يرجى ملاحظة أن بعض الخدمات التي يتم عرضها على هذا الموقع قد لا تعمل بشكل صحيح إذا قمت برفض ملفات تعريف الارتباط، حيث أننا قد نقوم / لا نقوم بربط المعلومات التي نقوم بتخزينها في ملفات تعريف الارتباط بأي معلومات تعريف شخصية تقدمها على موقعنا.
+
+2- المعلومات الشخصية
+
+يلتزم موقع (منصة الكيمياء) بالمبادئ الرئيسية والقواعد العامة لحماية البيانات الشخصية، وكذلك المبادئ الرئيسية والقواعد العامة لمشاركة البيانات.
+
+3- الموقع الجغرافي
+
+قد يُطلب من المستخدم في بعض الأحيان الموافقة على تحديد الموقع الجغرافي له، للاستفادة من بعض الخدمات المقدمة في (منصة الكيمياء) وتطبيقها.
+
+
+
+ما هو الغرض من جمع البيانات؟
+
+استخدام الموقع لبروتوكول الإنترنت الخاص بك يساعد على تشخيص المشكلات التي تحدث في الخوادم الخاصة بها، ويساعدها أيضًا على إجراء الإحصاء اللازم لقياس استخدام الموقع (عدد الزوار ولغة جهاز الكمبيوتر الذي تستخدمه).
+
+
+
+انتقال آمن للمعلومات
+
+نحن نضمن لك بأن هذا الموقع يحوز على أعلى إجراءات الأمن لحماية المعلومات التي تقدمها له. الموقع يعمل على تشفير المعلومات التي تعتبر حساسة (مثل أرقام بطاقات الائتمان) أو البيانات التي يتعين الحفاظ على سريتها توافقاً مع المتطلبات القانونية.
+
+
+
+حماية المعلومات الشخصية
+
+لن تكون معلوماتك الشخصية متاحة إلا لموظفي الموقع الذين يحتاجون إلى الاطلاع عليها. ودون موافقة المستخدم، لن تكون تلك المعلومات متاحة للجمهور. وعلاوة على ذلك لن يتم تبادل، أو تداول، أي من تلك المعلومات، أو بيعها لأي طرف آخر من غير الموافقة المسبقة للمستخدم، كما لن يسمح بالوصول إلى المعلومات إلا للأشخاص المؤهلين والذين يتولون تقديم خدمات (منصة الكيمياء).
+
+
+
+حقوق المستخدم
+
+يحق لأي مستخدم يزود الموقع بمعلوماته الشخصية التالي:
+
+الوصول إلى المعلومات.
+
+الحصول على المعلومات.
+
+مراجعة وتعديل المعلومات.
+
+سحب الموافقة وحذف المعلومات.
+
+وذلك عبر إرسال رسالة إلكترونية إلى care@chemstage.com .
+
+التواصل معنا
+
+إذا كانت لديك أي أسئلة بخصوص سياسة الخصوصية الخاصة بنا، أو طلب مساعدة أو تقديم شكوى، يُرجى الاتصال بنا عبر قنوات الاتصال المذكورة في الموقع.
+
+
+
+التغييرات التي قد تطرأ على سياسة الخصوصية  
+
+
+    <a href="javascript:void(0)" class="back-to-top">
+        <i class="lni lni-chevron-up"> </i>
+    </a>
+    <!-- ====== Back To Top End ====== -->
+
+    <!-- ====== All Javascript Files ====== -->
+    <script src="{{ asset(main_path() . 'landing/assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset(main_path() . 'landing/assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset(main_path() . 'landing/assets/js/main.js') }}"></script>
+    <script>
+        // ==== for menu scroll
+        const pageLink = document.querySelectorAll(".ud-menu-scroll");
+
+        pageLink.forEach((elem) => {
+            elem.addEventListener("click", (e) => {
+                e.preventDefault();
+                document.querySelector(elem.getAttribute("href")).scrollIntoView({
+                    behavior: "smooth",
+                    offsetTop: 1 - 60,
+                });
+            });
+        });
+
+        // section menu active
+        function onScroll(event) {
+            const sections = document.querySelectorAll(".ud-menu-scroll");
+            const scrollPos =
+                window.pageYOffset ||
+                document.documentElement.scrollTop ||
+                document.body.scrollTop;
+
+            for (let i = 0; i < sections.length; i++) {
+                const currLink = sections[i];
+                const val = currLink.getAttribute("href");
+                const refElement = document.querySelector(val);
+                const scrollTopMinus = scrollPos + 73;
+                if (
+                    refElement.offsetTop <= scrollTopMinus &&
+                    refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+                ) {
+                    document
+                        .querySelector(".ud-menu-scroll")
+                        .classList.remove("active");
+                    currLink.classList.add("active");
+                } else {
+                    currLink.classList.remove("active");
+                }
+            }
+        }
+
+        window.document.addEventListener("scroll", onScroll);
+    </script>
+</body>
+
+</html>
+
+
+يمتلك موقع (منصة الكيمياء) حرية التصرف في تحديث سياسة الخصوصية هذه في أي وقت.
